f259810db0fd51e0696fecff27d01b18df11eb71
Karan <kb466444@gmail.com>
2025-07-13 15:47:02 +0530

    Error Fixd

--- app/api/admin/issues/update/[id]/route.ts
+++ app/api/admin/issues/update/[id]/route.ts
@@ -2,10 +2,7 @@ import { NextRequest, NextResponse } from "next/server";
 import { connectDB } from "@/lib/mongodb";
 import { Issue } from "@/models/Issue";
 
-export async function PATCH(
-  req: NextRequest,
-  context: { params: { id: string } }
-) {
+export async function PATCH(req: NextRequest, context: any) {
   await connectDB();
   const { status } = await req.json();
 
